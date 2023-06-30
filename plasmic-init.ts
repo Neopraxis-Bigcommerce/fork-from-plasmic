a5e3af6d3d5767c70aa598565a882788904437d4
Subhashneopraxis <subash.surendran@neopraxis.in>
2023-06-30 14:24:02 +0530

    change plasmic-init file

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,26 +1,12 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
-
-const PLASMIC_PROJECT_ID = process.env["jTKbAFLJzNqzk5Sx157Ti8"];
-const PLASMIC_PROJECT_API_TOKEN =
-  process.env["KFchRf4qByVG7ZtP3nWWpzgKZK7VvvZCpqNOSPIBZaDVLgmvEKMf6hv1ySIRQCQGlU0WFosCNU0pVojBLQ"];
-
-const PLASMIC_CONFIG = {
+export const PLASMIC = initPlasmicLoader({
   projects: [
     {
-      id: 'jTKbAFLJzNqzk5Sx157Ti8',
-      token: 'KFchRf4qByVG7ZtP3nWWpzgKZK7VvvZCpqNOSPIBZaDVLgmvEKMf6hv1ySIRQCQGlU0WFosCNU0pVojBLQ',
-    },
+      id: "jTKbAFLJzNqzk5Sx157Ti8",  // ID of a project you are using
+      token: "KFchRf4qByVG7ZtP3nWWpzgKZK7VvvZCpqNOSPIBZaDVLgmvEKMf6hv1ySIRQCQGlU0WFosCNU0pVojBLQ"  // API token for that project
+    }
   ],
-};
-
-export const PLASMIC = initPlasmicLoader({
-  ...PLASMIC_CONFIG,
-  preview: true,
-});
-
-export const PREVIEW_PLASMIC = initPlasmicLoader({
-  ...PLASMIC_CONFIG,
   // Fetches the latest revisions, whether or not they were unpublished!
   // Disable for production to ensure you render only published changes.
   preview: true,
-});
+})
